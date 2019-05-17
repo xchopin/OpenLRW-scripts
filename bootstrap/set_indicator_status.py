615118cab829d245b462b5c9f2578a03ae7a4d88
xchopin <xchopin54@gmail.com>
2019-05-17 14:55:53 +0200

    fix: helpers import

--- bootstrap/set_indicator_status.py
+++ bootstrap/set_indicator_status.py
@@ -12,7 +12,7 @@ import os
 import sys
 import requests
 
-from bootstrap.helpers import *
+from helpers import *
 
 logging.basicConfig(filename=os.path.dirname(__file__) + '/maintenance_mode.log', level=logging.ERROR)
 
