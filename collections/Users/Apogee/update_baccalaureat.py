baaf55734c1764fef22dc0605222a8524860eac4
xchopin <xchopin54@gmail.com>
2019-06-11 12:07:59 +0200

    feat: add try except on api calls

--- collections/Users/Apogee/update_baccalaureat.py
+++ collections/Users/Apogee/update_baccalaureat.py
@@ -12,12 +12,30 @@ __status__ = "Production"
 import json
 import sys, os
 import csv
+import requests
 sys.path.append(os.path.dirname(__file__) + '/../../..')
 from bootstrap.helpers import *
 
 jwt = OpenLrw.generate_jwt()  # Generate a JSON Web Token for using OneRoster routes
 
+
+def exit_log(user_id, reason):
+    """
+    Stops the script and email + logs the last event
+    :param method:
+    :param user_id:
+    :param reason:
+    """
+    subject = str(sys.argv[0])
+    message = "An error occured on user" + str(user_id) + "\n\n Details: \n" + str(reason)
+    OpenLrw.mail_server(subject, message)
+    logging.error(message)
+    OpenLrw.pretty_error("Error", "User " + str(user_id))
+    sys.exit(0)
+
+
 csv_file = open('data/Users/baccalaureat_student.csv', 'r')
+
 counter = 0
 with csv_file:
     has_header = csv.Sniffer().has_header(csv_file.read(1024))
@@ -29,16 +47,31 @@ with csv_file:
         user_id, serie_bac, year, mention = row
         if year == '' or serie_bac == '':
             continue
-        user = OpenLrw.get_user(user_id, jwt)
+
+        try:
+            user = OpenLrw.get_user(user_id, jwt)
+        except ExpiredTokenException:
+            JWT = OpenLrw.generate_jwt()
+            user = OpenLrw.get_user(user_id, jwt)
+
         if user:
             user = json.loads(user)
             user["metadata"]["bac_serie"] = serie_bac
             user["metadata"]["bac_annee"] = year
             user["metadata"]["bac_mention"] = mention
 
-            OpenLrw.post_user(user, jwt, True)  # Replace the user with the new value
+            try:
+                OpenLrw.post_user(user, jwt, True)  # Replace the user with the new value
+            except ExpiredTokenException:
+                JWT = OpenLrw.generate_jwt()
+                OpenLrw.post_user(user, jwt, True)  # Replace the user with the new value
+            except InternalServerErrorException as e:
+                exit_log('Unable to update the user ' + user_id, e.message.content)
+            except requests.exceptions.ConnectionError as e:
+                exit_log('Unable to update the user ' + user_id, e)
+
             counter += 1
 
     time = measure_time()
 
-    OpenLrw.mail_server(sys.argv[0] + " executed", str(counter) + " users edited in " + measure_time() + " seconds")
+    OpenLrw.mail_server(sys.argv[0] + " executed", str(counter) + " users updated in " + measure_time() + " seconds")
