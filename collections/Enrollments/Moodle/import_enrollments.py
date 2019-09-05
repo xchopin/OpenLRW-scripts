9c3ffb7340d3677e6e97177395ec16bdc182a1b9
xchopin <xchopin54@gmail.com>
2019-09-05 15:13:08 +0200

    feat: add -u argument for updating the enrollments collection

--- collections/Enrollments/Moodle/import_enrollments.py
+++ collections/Enrollments/Moodle/import_enrollments.py
@@ -4,7 +4,7 @@
 __author__ = "Xavier Chopin"
 __copyright__ = "Copyright 2019, University of Lorraine"
 __license__ = "ECL-2.0"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __email__ = "xavier.chopin@univ-lorraine.fr"
 __status__ = "Production"
 
@@ -15,6 +15,7 @@ import os
 import requests
 import datetime
 import re
+import json
 
 sys.path.append(os.path.dirname(__file__) + '/../../..')
 from bootstrap.helpers import *
@@ -23,7 +24,9 @@ logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%
 
 
 parser = OpenLRW.parser
-parser.add_argument('-f', '--from', required='True', action='store', help='Timestamp (FROM) for querying Moodle`s database')
+parser.add_argument('-f', '--from',  action='store', help='Timestamp (from) for querying Moodle`s database')
+parser.add_argument('-u', '--update', action='store_true', help='Import newer enrollments than the last one stored in mongo')
+
 args = vars(OpenLRW.enable_argparse())
 
 
@@ -57,15 +60,35 @@ db = MySQLdb.connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME)
 query = db.cursor()
 
 # ----- MAIN -------
+if (args['from'] is None) and (args['update'] is False):
+    OpenLRW.pretty_error("Wrong usage", ["This script requires an argument, please run --help to get more details"])
+    exit()
 
-if not re.match(TIMESTAMP_REGEX, args['from']):
-    OpenLRW.pretty_error("Wrong usage", ["Argument must be a timestamp (from)"])
 
-query.execute("SELECT assignment.id, user.username, context.instanceid, assignment.userid, assignment.roleid  "
+if args['from'] is not None:
+    if not re.match(TIMESTAMP_REGEX, args['from']):
+        OpenLRW.pretty_error("Wrong usage", ["Argument must be a timestamp (from)"])
+    else:
+        sql_where = ">= " + args['from']
+
+elif args['update'] is True:
+    jwt = OpenLrw.generate_jwt()
+    last_enrollment = OpenLrw.http_auth_get('/api/enrollments?orderBy=begindate&page=0&limit=1', jwt)
+    if last_enrollment is None:
+        OpenLrw.pretty_error("Error", "There is no enrollments")
+        OpenLrw.mail_server("Subject: Error", "Either OpenLRW is turned off either, there is no enrollments")
+        exit()
+    last_enrollment = json.loads(last_enrollment)[0]
+    date = datetime.datetime.strptime(last_enrollment['beginDate'], '%Y-%m-%dT%H:%M:%SZ')
+    query_timestamp = (date - datetime.datetime(1970, 1, 1)).total_seconds()
+    sql_where = "> " + str(query_timestamp)
+
+
+query.execute("SELECT assignment.id, user.username, context.instanceid, assignment.userid, assignment.roleid, assignment.timemodified  "
               "FROM mdl_role_assignments as assignment, mdl_context as context, mdl_user as user "
               "WHERE context.id = assignment.contextid "
               "AND user.id = assignment.userid AND (assignment.roleid = 3 OR assignment.roleid = 4 OR assignment.roleid = 5)"
-              "AND assignment.timemodified >= " + args['fr'])
+              "AND assignment.timemodified " + sql_where
 
 enrollments = query.fetchall()
 
@@ -74,13 +97,14 @@ JWT = OpenLrw.generate_jwt()
 OpenLRW.pretty_message('Info', 'Executing...')
 
 for enrollment in enrollments:
-    enrollment_id, username, class_id, user_id, role = enrollment
+    enrollment_id, username, class_id, user_id, role, timestamp = enrollment
     json = {
         'sourcedId': enrollment_id,
         'role': 'student' if role == 5 else 'teacher',
         'user': {
             'sourcedId': username,
         },
+        'beginDate': timestamp,
         'primary': True,
         'status': 'active'
     }
